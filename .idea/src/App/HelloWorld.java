e3f85182b6de8cb395f40e179310e0e11c4c9196
IsmElMe <ismelemed@alu.edu.gva.es>
2023-03-13 16:44:41 +0100

    Creado un método sumar

--- .idea/src/App/HelloWorld.java
+++ .idea/src/App/HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println(sumar(10, 5));
     }
 
     public static int sumar(int x, int y) {
